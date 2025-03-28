34563c58f4403f5aa6b7e84fe2c942930ac01c31
traaitt | TRRXITTE <traaitt@icloud.com>
2025-03-28 03:11:13 +0100

    rename folder

--- documentation/swagger-initializer.js
+++ documentation/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://scrypt.trrxitte.com/api-docs/swagger.json",
+    url: "https://scrypt.trrxitte.com/documentation/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
